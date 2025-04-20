01365ac6e1db8a82cdad61b8a872291383c1b72c
Ava Tran <ava.tran1011@gmail.com>
2025-04-19 22:56:55 -0400

    rendering TourCards

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,27 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-const TourCard = ({id, image, info, price, name, onRemove}) =>
-{
-    const [readMore, setReadMore]  = useState(false);
-    return(
-        <article className="tour-card">
-       <img src={image} alt={name} />
-       <footer>
-         <div className="tour-info">
-           <h4>{name}</h4>
-           <h4 className="tour-price">${price}</h4>
-         </div>
-         <p>
-           {readMore ? info : `${info.substring(0, 150)}...`}
-           <button onClick={() => setReadMore(!readMore)}>
-             {readMore ? 'Show Less' : 'Read More'}
-           </button>
-         </p>
-         <button className="btn" onClick={() => onRemove(id)}>
-           Not Interested
-         </button>
-       </footer>
-     </article>
-    );
-}
+// TourCard component displays individual tour details
+const TourCard = ({ id, image, info, price, name, onRemove }) => {
+  const [readMore, setReadMore] = useState(false); // State to toggle full description
+
+  return (
+    <article className="tour-card">
+      <img src={image} alt={name} />  {/* Display tour image */}
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>  {/* Display tour name and price */}
+        </div>
+        <p>
+          {/* button that gives the option to read more of the tour's desciption */}
+          {readMore ? info : `${info.substring(0, 150)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? 'show less!' : 'read more!'}
+          </button>
+        </p>
+        {/* Button that will remove the tour*/}
+        <button className="btn" onClick={() => onRemove(id)}>not interested</button>
+      </footer>
+    </article>
+  );
+};
+
 export default TourCard;
